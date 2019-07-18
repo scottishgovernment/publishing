3cc6d70443c6ae5fe7f890aed8c641cd8ef1e07d
Martin Ellis <martin.ellis@gov.scot>
2019-07-18 10:27:18 +0100

    Add project files from Hippo 13.2.0 archetype

---
+++ site/components/src/main/java/scot/mygov/publishing/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package scot.mygov.publishing.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="publishing:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
