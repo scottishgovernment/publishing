3c9161151f5c01cda72fcf3beb44bd2ce3a9934b
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2019-11-15 10:02:54 +0000

    MGS-5624 add "warning text" and "inset text" DS components to CKEditor

---
+++ cms/src/main/resources/ckeditor/plugins/warning-text/plugin.js
@@ -0,0 +1,42 @@
+CKEDITOR.plugins.add('warning-text', {
+    requires: 'widget',
+    icons: 'warning-text',
+    init: function (editor) {
+        editor.widgets.add('warning-text', {
+            button: 'Add warning text',
+
+            template: `<div class="ds_warning-text">
+                <strong class="ds_warning-text__icon" aria-hidden="true">!</strong>
+                <strong class="visually-hidden">Warning</strong>
+                <div class="ds_warning-text__text">
+
+                </div>
+            </div>`,
+
+            editables: {
+                content: {
+                    selector: '.ds_warning-text__text',
+                    pathName: 'warning'
+                },
+            },
+
+            allowedContent:
+            'strong(!ds_warning-text__icon); strong(!visually-hidden); div(!ds_warning-text__text)',
+
+            upcast: function( element ) {
+                return element.name == 'div' && element.hasClass( 'ds_warning-text' );
+            }
+        });
+    },
+
+    onLoad: function () {
+        if (document.getElementById('warningTextStyles') === null) {
+            let link = document.createElement('link');
+            link.id = 'warningTextStyles';
+            link.rel = 'stylesheet';
+            link.type = 'text/css';
+            link.href = this.path + 'warning-text.css';
+            document.body.appendChild(link);
+        }
+    }
+});
