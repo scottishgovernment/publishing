671732b7b839130b003741b982cf37cfbde052fe
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2019-11-15 15:19:11 +0000

    (Sonar) remove unused imports

--- cms/src/main/java/scot/mygov/publishing/plugins/ExtendedEditorPlugin.java
+++ cms/src/main/java/scot/mygov/publishing/plugins/ExtendedEditorPlugin.java
@@ -2,16 +2,11 @@ package scot.mygov.publishing.plugins;
 
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
-import org.apache.wicket.model.IModel;
-import org.hippoecm.frontend.editor.builder.FieldEditor;
 import org.hippoecm.frontend.editor.editor.EditorPlugin;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
-import org.hippoecm.frontend.types.IFieldDescriptor;
-import org.hippoecm.frontend.types.ITypeDescriptor;
 
 public class ExtendedEditorPlugin extends EditorPlugin {
-
 
     public ExtendedEditorPlugin(IPluginContext context, IPluginConfig config) {
         super(context, config);
