8362d3fcd501a1d5d41f337880de472cbdc7a75c
David Sinclair <david.sinclair@gov.scot>
2020-07-02 17:45:57 +0100

    Sonar

--- site/components/src/main/java/scot/mygov/publishing/beans/Sector.java
+++ site/components/src/main/java/scot/mygov/publishing/beans/Sector.java
@@ -1,7 +1,6 @@
 package scot.mygov.publishing.beans;
 
 import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 
 @HippoEssentialsGenerated(internalName = "publishing:sector")
