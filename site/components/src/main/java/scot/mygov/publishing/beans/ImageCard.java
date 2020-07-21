37c2c769cc0489304e4d408ed9f14c426aff624d
David Sinclair <david.sinclair@gov.scot>
2020-07-21 14:29:17 +0100

    Make all image variant names be lowercase to fix eap images

--- site/components/src/main/java/scot/mygov/publishing/beans/ImageCard.java
+++ site/components/src/main/java/scot/mygov/publishing/beans/ImageCard.java
@@ -8,99 +8,99 @@ import org.hippoecm.hst.content.beans.standard.HippoGalleryImageBean;
 @HippoEssentialsGenerated(internalName = "publishing:ImageCard")
 @Node(jcrType = "publishing:ImageCard")
 public class ImageCard extends HippoGalleryImageSet {
-    @HippoEssentialsGenerated(internalName = "publishing:cardSixteenNineS")
+    @HippoEssentialsGenerated(internalName = "publishing:cardsixteennines")
     public HippoGalleryImageBean getCardSixteenNineS() {
-        return getBean("publishing:cardSixteenNineS",
+        return getBean("publishing:cardsixteennines",
                 HippoGalleryImageBean.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publishing:cardSixteenNineM")
+    @HippoEssentialsGenerated(internalName = "publishing:cardsixteenninem")
     public HippoGalleryImageBean getCardSixteenNineM() {
-        return getBean("publishing:cardSixteenNineM",
+        return getBean("publishing:cardsixteenninem",
                 HippoGalleryImageBean.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publishing:cardSixteenNineL")
+    @HippoEssentialsGenerated(internalName = "publishing:cardsixteenninel")
     public HippoGalleryImageBean getCardSixteenNineL() {
-        return getBean("publishing:cardSixteenNineL",
+        return getBean("publishing:cardsixteenninel",
                 HippoGalleryImageBean.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publishing:cardSixteenNineXl")
+    @HippoEssentialsGenerated(internalName = "publishing:cardsixteenninexl")
     public HippoGalleryImageBean getCardSixteenNineXl() {
-        return getBean("publishing:cardSixteenNineXl",
+        return getBean("publishing:cardsixteenninexl",
                 HippoGalleryImageBean.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publishing:cardSixteenNineSDbl")
+    @HippoEssentialsGenerated(internalName = "publishing:cardsixteenninesdbl")
     public HippoGalleryImageBean getCardSixteenNineSDbl() {
-        return getBean("publishing:cardSixteenNineSDbl",
+        return getBean("publishing:cardsixteenninesdbl",
                 HippoGalleryImageBean.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publishing:cardSixteenNineMDbl")
+    @HippoEssentialsGenerated(internalName = "publishing:cardsixteenninemdbl")
     public HippoGalleryImageBean getCardSixteenNineMDbl() {
-        return getBean("publishing:cardSixteenNineMDbl",
+        return getBean("publishing:cardsixteenninemdbl",
                 HippoGalleryImageBean.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publishing:cardSixteenNineLDbl")
+    @HippoEssentialsGenerated(internalName = "publishing:cardsixteennineldbl")
     public HippoGalleryImageBean getCardSixteenNineLDbl() {
-        return getBean("publishing:cardSixteenNineLDbl",
+        return getBean("publishing:cardsixteennineldbl",
                 HippoGalleryImageBean.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publishing:cardSixteenNineXlDbl")
+    @HippoEssentialsGenerated(internalName = "publishing:cardsixteenninexldbl")
     public HippoGalleryImageBean getCardSixteenNineXlDbl() {
-        return getBean("publishing:cardSixteenNineXlDbl",
+        return getBean("publishing:cardsixteenninexldbl",
                 HippoGalleryImageBean.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publishing:cardTwoThreeFiveS")
+    @HippoEssentialsGenerated(internalName = "publishing:cardtwothreefives")
     public HippoGalleryImageBean getCardTwoThreeFiveS() {
-        return getBean("publishing:cardTwoThreeFiveS",
+        return getBean("publishing:cardtwothreefives",
                 HippoGalleryImageBean.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publishing:cardTwoThreeFiveM")
+    @HippoEssentialsGenerated(internalName = "publishing:cardtwothreefivem")
     public HippoGalleryImageBean getCardTwoThreeFiveM() {
-        return getBean("publishing:cardTwoThreeFiveM",
+        return getBean("publishing:cardtwothreefivem",
                 HippoGalleryImageBean.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publishing:cardTwoThreeFiveL")
+    @HippoEssentialsGenerated(internalName = "publishing:cardtwothreefivel")
     public HippoGalleryImageBean getCardTwoThreeFiveL() {
-        return getBean("publishing:cardTwoThreeFiveL",
+        return getBean("publishing:cardtwothreefivel",
                 HippoGalleryImageBean.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publishing:cardTwoThreeFiveXl")
+    @HippoEssentialsGenerated(internalName = "publishing:cardtwothreefivexl")
     public HippoGalleryImageBean getCardTwoThreeFiveXl() {
-        return getBean("publishing:cardTwoThreeFiveXl",
+        return getBean("publishing:cardtwothreefivexl",
                 HippoGalleryImageBean.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publishing:cardTwoThreeFiveSDbl")
+    @HippoEssentialsGenerated(internalName = "publishing:cardtwothreefivesdbl")
     public HippoGalleryImageBean getCardTwoThreeFiveSDbl() {
-        return getBean("publishing:cardTwoThreeFiveSDbl",
+        return getBean("publishing:cardtwothreefivesdbl",
                 HippoGalleryImageBean.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publishing:cardTwoThreeFiveMDbl")
+    @HippoEssentialsGenerated(internalName = "publishing:cardtwothreefivemdbl")
     public HippoGalleryImageBean getCardTwoThreeFiveMDbl() {
-        return getBean("publishing:cardTwoThreeFiveMDbl",
+        return getBean("publishing:cardtwothreefivemdbl",
                 HippoGalleryImageBean.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publishing:cardTwoThreeFiveLDbl")
+    @HippoEssentialsGenerated(internalName = "publishing:cardtwothreefiveldbl")
     public HippoGalleryImageBean getCardTwoThreeFiveLDbl() {
-        return getBean("publishing:cardTwoThreeFiveLDbl",
+        return getBean("publishing:cardtwothreefiveldbl",
                 HippoGalleryImageBean.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publishing:cardTwoThreeFiveXlDbl")
+    @HippoEssentialsGenerated(internalName = "publishing:cardtwothreefivexldbl")
     public HippoGalleryImageBean getCardTwoThreeFiveXlDbl() {
-        return getBean("publishing:cardTwoThreeFiveXlDbl",
+        return getBean("publishing:cardtwothreefivexldbl",
                 HippoGalleryImageBean.class);
     }
 }
