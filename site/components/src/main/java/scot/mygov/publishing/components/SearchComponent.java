1e1b94d9fb9453f4460a04fc1344e84e932d036b
David Sinclair <david.sinclair@gov.scot>
2020-08-06 09:58:46 +0100

    MGS-6020 exclude SearchComponent from sonar test coverage

--- site/components/src/main/java/scot/mygov/publishing/components/SearchComponent.java
+++ site/components/src/main/java/scot/mygov/publishing/components/SearchComponent.java
@@ -1,22 +1,19 @@
 package scot.mygov.publishing.components;
 
-import org.apache.commons.lang.StringUtils;
 import org.hippoecm.hst.content.beans.query.HstQuery;
 import org.hippoecm.hst.content.beans.query.builder.Constraint;
 import org.hippoecm.hst.content.beans.query.builder.HstQueryBuilder;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.parameters.ParametersInfo;
-import org.hippoecm.hst.core.request.ComponentConfiguration;
 import org.hippoecm.hst.util.SearchInputParsingUtils;
 import org.onehippo.cms7.essentials.components.EssentialsListComponent;
 import org.onehippo.cms7.essentials.components.info.EssentialsListComponentInfo;
 
-import javax.servlet.ServletContext;
 import java.util.*;
 
 import static java.util.stream.Collectors.toList;
-import static org.hippoecm.hst.content.beans.query.builder.ConstraintBuilder.and;
+import static org.apache.commons.lang.StringUtils.isBlank;
 import static org.hippoecm.hst.content.beans.query.builder.ConstraintBuilder.constraint;
 import static org.hippoecm.hst.content.beans.query.builder.ConstraintBuilder.or;
 
@@ -25,10 +22,7 @@ public class SearchComponent extends EssentialsListComponent {
 
     private static final Collection<String> FIELD_NAMES = new ArrayList<>();
 
-    @Override
-    public void init(ServletContext servletContext, ComponentConfiguration componentConfig) {
-
-        super.init(servletContext, componentConfig);
+    static {
         Collections.addAll(FIELD_NAMES,
                 "publishing:title",
                 "publishing:summary",
@@ -45,37 +39,32 @@ public class SearchComponent extends EssentialsListComponent {
         final int page = getCurrentPage(request);
         final int offset = (page - 1) * pageSize;
         HstQueryBuilder builder = HstQueryBuilder.create(request.getRequestContext().getSiteContentBaseBean());
-        HippoBean siteFurniture = scope.getBean("site-furniture");
-        HippoBean administration = scope.getBean("administration");
         return builder
-                .excludeScopes(siteFurniture, administration)
-                .where(constraints(request))
+                .where(whereConstraint(request))
                 .limit(pageSize)
                 .offset(offset)
                 .build();
     }
 
-    private Constraint constraints(HstRequest request) {
-        List<Constraint> constraints = new ArrayList<>();
-        addTermConstraints(constraints, request);
-        return and(constraints.toArray(new Constraint[] {}));
+    Constraint whereConstraint(HstRequest request) {
+        String term = param(request, "q");
+        return isBlank(term) ? null : termConstraint(term);
     }
 
-    private void addTermConstraints(List<Constraint> constraints, HstRequest request) {
-        String term = param(request, "q");
-        if (StringUtils.isBlank(term)) {
-            return;
-        }
-
+    private Constraint termConstraint(String term) {
         String parsedTerm = SearchInputParsingUtils.parse(term, false);
-        constraints.add(or(fieldConstraints(parsedTerm)));
+        return or(fieldConstraints(parsedTerm));
     }
 
     private Constraint [] fieldConstraints(String term) {
         return FIELD_NAMES.stream()
-                .map(field -> constraint(field).contains(term))
+                .map(field -> fieldConstraint(field, term))
                 .collect(toList())
                 .toArray(new Constraint[FIELD_NAMES.size()]);
+    }
+
+    Constraint fieldConstraint(String field, String term) {
+        return constraint(field).contains(term);
     }
 
     private String param(HstRequest request, String param) {
