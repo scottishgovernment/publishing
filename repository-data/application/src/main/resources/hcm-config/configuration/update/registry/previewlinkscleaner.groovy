b66a0ad09a5abb7af0622e7f510dd00cc7580ea8
Lef Karamoulas <lef.karamoulas@bloomreach.com>
2020-09-02 12:43:24 +0300

    MGS-6051 : Update groovy

--- repository-data/application/src/main/resources/hcm-config/configuration/update/registry/previewlinkscleaner.groovy
+++ repository-data/application/src/main/resources/hcm-config/configuration/update/registry/previewlinkscleaner.groovy
@@ -7,22 +7,16 @@ import javax.jcr.Session
 class PreviewLinksCleaner extends BaseNodeUpdateVisitor {
 
     String defaultMode = "all";
-    int defaultTime = 30;
-
     String mode;
-    int time;
 
     void initialize(Session session) {
         mode = parametersMap.get("mode", defaultMode)
-        time = parametersMap.get("period", defaultTime)
 
-        if (!mode.equals("all") && !mode.equals("invalid")) {
+        if (!mode.equals("all") && !mode.equals("expired")) {
             mode = defaultMode
         }
-        if (time < 0) {
-            time = defaultTime
-        }
-        log.info "PreviewLinksCleaner initialized with parameters: { mode: ${mode}, time: ${time} }"
+
+        log.info "PreviewLinksCleaner initialized with parameters: { mode: ${mode} }"
 
     }
 
@@ -36,7 +30,7 @@ class PreviewLinksCleaner extends BaseNodeUpdateVisitor {
         Calendar expirationCalendar = JcrUtils.getDateProperty(node, "staging:expirationdate", null)
         expirationCalendar == null || Calendar.getInstance().before(expirationCalendar)
 
-        if("all".equals(mode) || ("invalid".equals(mode) && expirationCalendar.after(Calendar.getInstance()))){
+        if("all".equals(mode) || ("expired".equals(mode) && expirationCalendar.after(Calendar.getInstance()))){
             shouldRemove = true
         }
 
