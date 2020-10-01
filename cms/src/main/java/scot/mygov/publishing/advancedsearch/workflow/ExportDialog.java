ae32042c2340c7fb344d0ed03ec1cab2fabd2ef3
Lef Karamoulas <lef.karamoulas@bloomreach.com>
2020-10-01 16:29:36 +0200

    MGS-6062 Improve exception handling & reuse docs count

--- cms/src/main/java/scot/mygov/publishing/advancedsearch/workflow/ExportDialog.java
+++ cms/src/main/java/scot/mygov/publishing/advancedsearch/workflow/ExportDialog.java
@@ -71,10 +71,11 @@ public class ExportDialog extends Dialog<WorkflowDescriptor> {
     public ExportDialog(final ISearchContext searcher) {
         super();
         this.searcher = searcher;
+        int documentsCount = getCountSelectedDocuments();
 
         Label countText = new Label("counttext", MessageFormat.format(
                 new StringResourceModel("export-confirmation-subtext",
-                        ExportDialog.this).getString(), getCountSelectedDocuments()
+                        ExportDialog.this).getString(), documentsCount
         ));
         add(countText);
 
@@ -82,7 +83,7 @@ public class ExportDialog extends Dialog<WorkflowDescriptor> {
 
             @Override
             public boolean isEnabled(){
-                return searcher.getSelectedDocuments().hasNext();
+                return documentsCount > 0;
             }
 
         };
@@ -141,17 +142,12 @@ public class ExportDialog extends Dialog<WorkflowDescriptor> {
         Iterator<Node> nodeIterator = searcher.getSelectedDocuments();
         while (nodeIterator.hasNext()) {
             Node currentNode = nodeIterator.next();
-
-            try {
-                List<String> propertiesList = constructPropertiesList(currentNode);
-                for (int i = 0; i < propertiesList.size(); i++) {
-                    appendValue(stringBuilder, i, propertiesList.get(i));
-                }
-                if(propertiesList.size()>0){
-                    stringBuilder.append(DEFAULT_LINE_TERMINATOR);
-                }
-            } catch (RepositoryException e){
-                LOG.error("An exception occurred while exporting CSV.", e);
+            List<String> propertiesList = constructPropertiesList(currentNode);
+            for (int i = 0; i < propertiesList.size(); i++) {
+                appendValue(stringBuilder, i, propertiesList.get(i));
+            }
+            if(propertiesList.size()>0){
+                stringBuilder.append(DEFAULT_LINE_TERMINATOR);
             }
         }
 
@@ -159,7 +155,7 @@ public class ExportDialog extends Dialog<WorkflowDescriptor> {
         return ArrayUtils.addAll(BOM, bytes);
     }
 
-    private List<String> constructPropertiesList(final Node handle) throws RepositoryException {
+    private List<String> constructPropertiesList(final Node handle) {
         ArrayList<String> props = new ArrayList<>();
         String nodeStateSummary = getNodeStateSummary(handle);
         Node variant;
@@ -171,62 +167,68 @@ public class ExportDialog extends Dialog<WorkflowDescriptor> {
                 variant = getDocumentVariantByHippoStdState(handle, UNPUBLISHED);
             }
             if (variant != null) {
-                switch (property) {
-                    case "url":
-                        List<String> urls = getDocumentSiteURL(variant);
-                        if (!urls.isEmpty()) {
-                            props.add(urls.get(0));
-                        } else {
-                            props.add("");
-                        }
-                        break;
-                    case "id":
-                        props.add(variant.getIdentifier());
-                        break;
-                    case "format":
-                        props.add(variant.getPrimaryNodeType().getName());
-                        break;
-                    case "publishing:reviewDate":
-                        if (variant.hasProperty(property)) {
-                            props.add(DateTimePrinter.of(variant.getProperty("publishing:reviewDate").getDate()).print());
-                        } else {
-                            props.add("");
-                        }
-                        break;
-                    case HIPPOSTDPUBWF_LAST_MODIFIED_BY:
-                        if ("changed".equals(nodeStateSummary)) {
-                            variant = getDocumentVariantByHippoStdState(handle, UNPUBLISHED);
-                        }
-                        if(variant != null && variant.hasProperty(HIPPOSTDPUBWF_LAST_MODIFIED_BY)){
-                            props.add(variant.getProperty(HIPPOSTDPUBWF_LAST_MODIFIED_BY).getString());
-                        } else {
-                            props.add("");
-                        }
-                        break;
-                    case HIPPOSTDPUBWF_LAST_MODIFIED_DATE:
-                        if ("changed".equals(nodeStateSummary)) {
-                            variant = getDocumentVariantByHippoStdState(handle, UNPUBLISHED);
-                        }
-                        if(variant != null && variant.hasProperty(HIPPOSTDPUBWF_LAST_MODIFIED_DATE)){
-                            props.add(DateTimePrinter.of(variant.getProperty(HIPPOSTDPUBWF_LAST_MODIFIED_DATE).getDate()).print());
-                        } else {
-                            props.add("");
-                        }
-                        break;
-                    case "publishing:contentOwner": case "publishing:factCheckers":
-                        if (variant.hasNode(property)) {
-                            props.add(extractContactEmails(variant.getNodes(property)));
-                        } else {
-                            props.add("");
-                        }
-                        break;
-                    default:
-                        if (variant.hasProperty(property)) {
-                            props.add(variant.getProperty(property).getString());
-                        } else {
-                            props.add("");
-                        }
-                        break;
+                try {
+                    switch (property) {
+                        case "url":
+                            List<String> urls = getDocumentSiteURL(variant);
+                            if (!urls.isEmpty()) {
+                                props.add(urls.get(0));
+                            } else {
+                                props.add("");
+                            }
+                            break;
+                        case "id":
+                            props.add(variant.getIdentifier());
+                            break;
+                        case "format":
+                            props.add(variant.getPrimaryNodeType().getName());
+                            break;
+                        case "publishing:reviewDate":
+                            if (variant.hasProperty(property)) {
+                                props.add(DateTimePrinter.of(variant.getProperty("publishing:reviewDate").getDate()).print());
+                            } else {
+                                props.add("");
+                            }
+                            break;
+                        case HIPPOSTDPUBWF_LAST_MODIFIED_BY:
+                            if ("changed".equals(nodeStateSummary)) {
+                                variant = getDocumentVariantByHippoStdState(handle, UNPUBLISHED);
+                            }
+                            if (variant != null && variant.hasProperty(HIPPOSTDPUBWF_LAST_MODIFIED_BY)) {
+                                props.add(variant.getProperty(HIPPOSTDPUBWF_LAST_MODIFIED_BY).getString());
+                            } else {
+                                props.add("");
+                            }
+                            break;
+                        case HIPPOSTDPUBWF_LAST_MODIFIED_DATE:
+                            if ("changed".equals(nodeStateSummary)) {
+                                variant = getDocumentVariantByHippoStdState(handle, UNPUBLISHED);
+                            }
+                            if (variant != null && variant.hasProperty(HIPPOSTDPUBWF_LAST_MODIFIED_DATE)) {
+                                props.add(DateTimePrinter.of(variant.getProperty(HIPPOSTDPUBWF_LAST_MODIFIED_DATE).getDate()).print());
+                            } else {
+                                props.add("");
+                            }
+                            break;
+                        case "publishing:contentOwner":
+                        case "publishing:factCheckers":
+                            if (variant.hasNode(property)) {
+                                props.add(extractContactEmails(variant.getNodes(property)));
+                            } else {
+                                props.add("");
+                            }
+                            break;
+                        default:
+                            if (variant.hasProperty(property)) {
+                                props.add(variant.getProperty(property).getString());
+                            } else {
+                                props.add("");
+                            }
+                            break;
+                    }
+                } catch (RepositoryException e){
+                    LOG.error("An exception occurred while exporting CSV for the property {} ", property, e);
+                    props.add("---Exception occurred---");
                 }
             }
         }
@@ -234,42 +236,54 @@ public class ExportDialog extends Dialog<WorkflowDescriptor> {
         return props;
     }
 
-    private String extractContactEmails(final NodeIterator nodeIterator) throws RepositoryException{
+    private String extractContactEmails(final NodeIterator nodeIterator){
         ArrayList<String> contactEmails = new ArrayList<>();
         while (nodeIterator.hasNext()){
             Node currentNode = nodeIterator.nextNode();
-            if(currentNode.hasProperty(HIPPO_DOCBASE)){
-                Node contactHandle = UserSession.get().getJcrSession().getNodeByIdentifier(currentNode.getProperty(HIPPO_DOCBASE).getString());
-                Node publishedVariant = getDocumentVariantByHippoStdState(contactHandle, PUBLISHED);
-                if(publishedVariant != null && publishedVariant.hasProperty("publishing:email")){
-                    contactEmails.add(publishedVariant.getProperty("publishing:email").getString());
+            try {
+                if (currentNode.hasProperty(HIPPO_DOCBASE)) {
+                    Node contactHandle = UserSession.get().getJcrSession().getNodeByIdentifier(currentNode.getProperty(HIPPO_DOCBASE).getString());
+                    Node publishedVariant = getDocumentVariantByHippoStdState(contactHandle, PUBLISHED);
+                    if (publishedVariant != null && publishedVariant.hasProperty("publishing:email")) {
+                        contactEmails.add(publishedVariant.getProperty("publishing:email").getString());
+                    }
                 }
+            } catch (RepositoryException e){
+                LOG.error("An exception occurred while trying to extra contact emails for one of the selected documents.", e);
             }
         }
         return String.join(", ", contactEmails);
     }
 
-    private String getNodeStateSummary(final Node handle) throws RepositoryException{
-        Node randomVariant = handle.getNodes(handle.getName()).nextNode();
-        if(randomVariant.hasProperty(HIPPOSTD_STATESUMMARY)){
-            return randomVariant.getProperty(HIPPOSTD_STATESUMMARY).getString();
+    private String getNodeStateSummary(final Node handle) {
+        try {
+            Node randomVariant = handle.getNodes(handle.getName()).nextNode();
+            if (randomVariant.hasProperty(HIPPOSTD_STATESUMMARY)) {
+                return randomVariant.getProperty(HIPPOSTD_STATESUMMARY).getString();
+            }
+        } catch (RepositoryException e){
+            LOG.error("An exception occurred while trying to retrieve variant state summary.", e);
         }
         return null;
     }
 
-    private Node getDocumentVariantByHippoStdState(final Node handle, final String hippoStdState) throws RepositoryException {
+    private Node getDocumentVariantByHippoStdState(final Node handle, final String hippoStdState) {
         Node variantNode = null;
         String state;
 
-        for (NodeIterator nodeIt = handle.getNodes(handle.getName()); nodeIt.hasNext();) {
-            variantNode = nodeIt.nextNode();
+        try {
+            for (NodeIterator nodeIt = handle.getNodes(handle.getName()); nodeIt.hasNext(); ) {
+                variantNode = nodeIt.nextNode();
 
-            if (variantNode.hasProperty(HippoStdNodeType.HIPPOSTD_STATE)) {
-                state = variantNode.getProperty(HippoStdNodeType.HIPPOSTD_STATE).getString();
-                if (StringUtils.equals(hippoStdState, state)) {
-                    return variantNode;
+                if (variantNode.hasProperty(HippoStdNodeType.HIPPOSTD_STATE)) {
+                    state = variantNode.getProperty(HippoStdNodeType.HIPPOSTD_STATE).getString();
+                    if (StringUtils.equals(hippoStdState, state)) {
+                        return variantNode;
+                    }
                 }
             }
+        } catch (RepositoryException e){
+            LOG.error("An exception occurred while trying to retrieve the {} variant of a node.", hippoStdState, e);
         }
 
         return null;
