11439296c4855723c2e0d2757df63c183b74aa9e
Michael Irwin <michael.irwin@gov.scot>
2020-11-24 16:54:58 +0000

    sonar

--- site/components/src/main/java/scot/mygov/publishing/beans/DescribedLink.java
+++ site/components/src/main/java/scot/mygov/publishing/beans/DescribedLink.java
@@ -3,7 +3,6 @@ package scot.mygov.publishing.beans;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoCompound;
-import java.util.List;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
 
 @HippoEssentialsGenerated(internalName = "publishing:describedlink")
