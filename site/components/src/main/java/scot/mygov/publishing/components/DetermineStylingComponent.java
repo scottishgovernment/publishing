5d35cead262eb8da2cbce5106385ece91703ce1f
David Sinclair <david.sinclair@gov.scot>
2021-04-20 13:50:45 +0100

    Sonar

--- site/components/src/main/java/scot/mygov/publishing/components/DetermineStylingComponent.java
+++ site/components/src/main/java/scot/mygov/publishing/components/DetermineStylingComponent.java
@@ -3,7 +3,6 @@ package scot.mygov.publishing.components;
 import org.hippoecm.hst.configuration.hosting.Mount;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
-import org.onehippo.cms7.essentials.components.CommonComponent;
 import org.onehippo.cms7.essentials.components.EssentialsContentComponent;
 import scot.mygov.publishing.channels.WebsiteInfo;
 
