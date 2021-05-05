69fe5ff0a45c4c778d19673330e5c222754362a1
David Sinclair <david.sinclair@gov.scot>
2021-05-05 16:37:00 +0100

    Revert "MGS-6271 simplify DcoumentResourceContainer"
    This reverts commit 9497a259c9c3287f2e976c32e1f07237bf320287.

--- site/components/src/main/java/scot/mygov/publishing/linkprocessors/DocumentResourceContainer.java
+++ site/components/src/main/java/scot/mygov/publishing/linkprocessors/DocumentResourceContainer.java
@@ -1,6 +1,7 @@
 package scot.mygov.publishing.linkprocessors;
 
 import javax.jcr.Node;
+import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
@@ -10,6 +11,10 @@ import org.hippoecm.hst.core.linking.AbstractResourceContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import scot.mygov.publishing.HippoUtils;
+
+import static org.apache.commons.lang.StringUtils.equalsIgnoreCase;
+
 public class DocumentResourceContainer extends AbstractResourceContainer {
 
     private static final Logger LOG = LoggerFactory.getLogger(DocumentResourceContainer.class);
@@ -18,19 +23,30 @@ public class DocumentResourceContainer extends AbstractResourceContainer {
 
     private static final String HIPPO_FILENAME = "hippo:filename";
 
+    private HippoUtils hippoUtils = new HippoUtils();
+
     @Override
     public String getNodeType() {
         return PUBLISHING_DOCUMENT;
     }
 
     /**
-     * Add filenames to document and thumbnail urls
+     * Clean up urls for document cover page documents and thumbnails.
      */
     @Override
     public String resolveToPathInfo(Node resourceContainerNode, Node resourceNode, Mount mount) {
+
         try {
+            String pathInfo = resourceContainerNode.getPath();
+
+            if (pathInfo == null) {
+                return pathInfo;
+            }
+
             String filename = resourceNode.getProperty(HIPPO_FILENAME).getString();
-            return new StringBuilder(resourceNode.getPath()).append('/').append(filename).toString();
+            String path = resourceContainerNode.getParent().getParent().getPath();
+            path = StringUtils.substringAfter(path, "/content/documents");
+            return path + "/" + filename;
         } catch (RepositoryException e) {
             LOG.error("Exception processing a container resource link for a publishing:document node type.", e);
             return null;
@@ -39,7 +55,46 @@ public class DocumentResourceContainer extends AbstractResourceContainer {
 
     @Override
     public Node resolveToResourceNode(Session session, String pathInfo) {
-        String path = StringUtils.substringBeforeLast(pathInfo, "/");
-        return super.resolveToResourceNode(session, path);
+        int lastSlash = pathInfo.lastIndexOf('/');
+        String name = pathInfo.substring(lastSlash + 1);
+        String path = "/content/documents" + pathInfo.substring(0, lastSlash);
+
+        try {
+            if (!session.nodeExists(path)) {
+                return super.resolveToResourceNode(session, pathInfo);
+            }
+
+            Node handle = session.getNode(path);
+            Node publishedVariant = getVariant(handle);
+            return findNodeWithFilename(publishedVariant, name);
+        } catch (RepositoryException e) {
+            LOG.error("Exception processing a container resource link for a publishing:document node type.", e);
+        }
+
+        return super.resolveToResourceNode(session, pathInfo);
+    }
+
+    Node getVariant(Node handle) throws RepositoryException {
+        Node publishedVariant = hippoUtils.getPublishedVariant(handle);
+        return publishedVariant != null ? publishedVariant : handle.getNodes().nextNode();
+    }
+
+    Node findNodeWithFilename(Node coverPage, String name) throws RepositoryException {
+
+        // find either a document or a thumbnail
+        NodeIterator nodeIterator = coverPage.getNodes("publishing:documents*");
+        while (nodeIterator.hasNext()) {
+            Node documentNode = nodeIterator.nextNode();
+            Node resourceWithfilename = hippoUtils.find(documentNode.getNodes(), child -> hasFilename(child, name));
+            if (resourceWithfilename != null) {
+                return resourceWithfilename;
+            }
+        }
+        return null;
+    }
+
+    boolean hasFilename(Node node, String filename) throws RepositoryException {
+        String nodeFileName = node.getProperty(HIPPO_FILENAME).getString();
+        return equalsIgnoreCase(filename, nodeFileName);
     }
 }
