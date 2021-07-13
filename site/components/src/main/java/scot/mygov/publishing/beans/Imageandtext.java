62e16131bea02ae3b63661b31070c99edc5d3791
Gary Carmichael <gary.carmichael@gov.scot>
2021-07-13 18:12:03 +0100

    MGS-6375 Add image and text component improvements
    Changed content field to rich text HTML.
    Added variant template to support left and right alignment.
    Use HippoGalleryImageSet class in bean to allow images to show.

--- site/components/src/main/java/scot/mygov/publishing/beans/Imageandtext.java
+++ site/components/src/main/java/scot/mygov/publishing/beans/Imageandtext.java
@@ -1,14 +1,16 @@
 package scot.mygov.publishing.beans;
 
 import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoGalleryImageSet;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 
 @HippoEssentialsGenerated(internalName = "publishing:imageandtext")
 @Node(jcrType = "publishing:imageandtext")
 public class Imageandtext extends BaseDocument {
     @HippoEssentialsGenerated(internalName = "publishing:content")
-    public String getContent() {
-        return getSingleProperty("publishing:content");
+    public HippoHtml getContent() {
+        return getHippoHtml("publishing:content");
     }
 
     @HippoEssentialsGenerated(internalName = "publishing:alt")
@@ -17,7 +19,7 @@ public class Imageandtext extends BaseDocument {
     }
 
     @HippoEssentialsGenerated(internalName = "publishing:image")
-    public ImageDefault getImage() {
-        return getLinkedBean("publishing:image", ImageDefault.class);
+    public HippoGalleryImageSet getImage() {
+        return getLinkedBean("publishing:image", HippoGalleryImageSet.class);
     }
 }
