22db2b2b333e93dd756dfbf88b6095ca77e129d6
David Sinclair <david.sinclair@gov.scot>
2021-07-16 16:57:22 +0100

    Add divider component

---
+++ site/components/src/main/java/scot/mygov/publishing/components/DividerComponent.java
@@ -0,0 +1,10 @@
+package scot.mygov.publishing.components;
+
+import org.hippoecm.hst.core.parameters.ParametersInfo;
+import org.onehippo.cms7.essentials.components.CommonComponent;
+
+@ParametersInfo(type = DividerComponentInfo.class)
+public class DividerComponent extends CommonComponent {
+
+    // nothing required
+}
---
+++ site/components/src/main/java/scot/mygov/publishing/components/DividerComponentInfo.java
@@ -0,0 +1,6 @@
+package scot.mygov.publishing.components;
+
+import org.onehippo.cms7.essentials.components.info.EssentialsDocumentComponentInfo;
+
+public interface DividerComponentInfo {
+}
