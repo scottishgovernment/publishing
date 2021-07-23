3ff9c8e2d2877001c8fc7488b933cf3d9dc19b94
David Sinclair <david.sinclair@gov.scot>
2021-07-23 11:22:56 +0100

    seo tweaks - generated missing beans, fixed managecontent

---
+++ site/components/src/main/java/scot/mygov/publishing/beans/Seo.java
@@ -0,0 +1,13 @@
+package scot.mygov.publishing.beans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+
+@HippoEssentialsGenerated(internalName = "publishing:seo")
+@Node(jcrType = "publishing:seo")
+public class Seo extends Base {
+    @HippoEssentialsGenerated(internalName = "publishing:showFeedback")
+    public Boolean getShowFeedback() {
+        return getSingleProperty("publishing:showFeedback");
+    }
+}
