b1f64a7deabb4cff7ca54550f17bb74c21b49ab7
Katie Baigrie <katie.baigrie@gov.scot>
2021-11-05 08:58:28 +0000

    Sonar coverage

--- site/components/src/test/java/scot/mygov/publishing/components/SiteHeaderComponentTest.java
+++ site/components/src/test/java/scot/mygov/publishing/components/SiteHeaderComponentTest.java
@@ -46,6 +46,32 @@ public class SiteHeaderComponentTest {
 
     }
 
+    @Test
+    public void noChannelLogo() throws Exception {
+        // ARRANGE
+        Mount mount = mock(Mount.class);
+        ResolvedMount resolvedMount = mock(ResolvedMount.class);
+        WebsiteInfo info = mock(WebsiteInfo.class);
+        HstRequest request = mock(HstRequest.class);
+        HstRequestContext context = mock(HstRequestContext.class);
+        Session session = mock(Session.class);
+        ObjectBeanManager manager = mock(ObjectBeanManager.class);
+        Object object = mock(Object.class);
+
+        when(request.getRequestContext()).thenReturn(context);
+        when(context.getResolvedMount()).thenReturn(resolvedMount);
+        when(resolvedMount.getMount()).thenReturn(mount);
+        when(mount.getChannelInfo()).thenReturn(info);
+        when(info.getLogoPath()).thenReturn("");
+        when(context.getSession()).thenReturn(session);
+        when(context.getObjectBeanManager(session)).thenReturn(manager);
+        when(manager.getObject(anyString())).thenReturn(object);
+
+        // ACT
+        SiteHeaderComponent.determineLogo(request);
+
+    }
+
     @Test(expected = HstComponentException.class)
     public void exceptionRethrownAsHstComponentException() throws Exception {
         // ARRANGE
