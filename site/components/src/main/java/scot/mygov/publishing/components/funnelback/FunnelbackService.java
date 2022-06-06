e50bd4c9328f38f1234f936c2fbde235f56f5bdf
David Sinclair <david.sinclair@gov.scot>
2022-06-06 16:18:55 +0100

    suggestions tweak - only return array of strings

--- site/components/src/main/java/scot/mygov/publishing/components/funnelback/FunnelbackService.java
+++ site/components/src/main/java/scot/mygov/publishing/components/funnelback/FunnelbackService.java
@@ -21,6 +21,8 @@ import scot.mygov.publishing.components.funnelback.postprocessing.PostProcessor;
 
 import java.util.*;
 
+import static java.util.stream.Collectors.toList;
+
 @Service
 public class FunnelbackService {
 
@@ -28,14 +30,19 @@ public class FunnelbackService {
 
     private static final String FUNNELBACK = "funnelback";
 
+    // see https://community.funnelback.com/knowledge-base/implementation/search-interface/auto-completion/auto-completion-web-service-suggest.json
+    // fmt=json++ specifies that full result is returned, sort = 0 indicates to sort by score.
+    private static final String SUGGEST_URL = "/suggest.json?partial_query={partial_query}&collection=govscot~sp-mygov&fmt=json++&show=6&sort=0";
+
     FallbackService fallbackService = new FallbackService();
 
-    public Collection<Suggestion> getSuggestions(String partialQuery) {
+    public List<String> getSuggestions(String partialQuery) {
         Map<String, Object> params = Collections.singletonMap("partial_query", partialQuery);
         ResourceServiceBroker broker = CrispHstServices.getDefaultResourceServiceBroker(HstServices.getComponentManager());
-        Resource results = broker.findResources(FUNNELBACK, "/suggest.json?partial_query={partial_query}&collection=govscot~sp-mygov&fmt=json++&show=6&sort=0", params);
+        Resource results = broker.findResources(FUNNELBACK, SUGGEST_URL, params);
         ResourceBeanMapper resourceBeanMapper = broker.getResourceBeanMapper(FUNNELBACK);
-        return resourceBeanMapper.mapCollection(results.getChildren(), Suggestion.class);
+        Collection<Suggestion> suggestions = resourceBeanMapper.mapCollection(results.getChildren(), Suggestion.class);
+        return suggestions.stream().map(Suggestion::getDisp).collect(toList());
     }
 
     @HystrixCommand(
