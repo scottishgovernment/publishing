41c49a366cfbaab736269121eb1e240d0a908fb2
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2022-08-18 13:05:05 +0100

    Remove unused import

--- site/components/src/main/java/scot/mygov/publishing/beans/Home.java
+++ site/components/src/main/java/scot/mygov/publishing/beans/Home.java
@@ -3,7 +3,6 @@ package scot.mygov.publishing.beans;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoHtml;
-import org.hippoecm.hst.content.beans.standard.HippoGalleryImageSet;
 import java.util.Calendar;
 import java.util.List;
 import scot.mygov.publishing.beans.DescribedLink;
