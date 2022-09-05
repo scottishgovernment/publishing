971f054d5e61c2d3b0d3793d264ae2fdfbb703c6
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2022-09-05 16:14:54 +0100

    MGS- 6937 Checkbox smart answer question type
    - Smart answer has four question types:
      - multiple choice (current, to be migrated away from)
      - multi-select (checkboxes)
      - single select (radios/dropdown)
      - confirmation (with or without checkbox)
    
    - Checkboxes can have multiple exclusive options. The template places them at the end of the list of options.
    
    - Pattern library bumped to latest version, to take in the addition of exclusive checkboxes

---
+++ site/components/src/main/java/scot/mygov/publishing/beans/Smartanswersingleselectoption.java
@@ -0,0 +1,26 @@
+package scot.mygov.publishing.beans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoCompound;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
+
+@HippoEssentialsGenerated(internalName = "publishing:smartanswersingleselectoption")
+@Node(jcrType = "publishing:smartanswersingleselectoption")
+public class Smartanswersingleselectoption extends HippoCompound {
+    @HippoEssentialsGenerated(internalName = "publishing:value")
+    public String getValue() {
+        return getSingleProperty("publishing:value");
+    }
+
+    @HippoEssentialsGenerated(internalName = "publishing:label")
+    public String getLabel() {
+        return getSingleProperty("publishing:label");
+    }
+
+    @HippoEssentialsGenerated(internalName = "publishing:nextPage")
+    public HippoBean getNextPage() {
+        return getLinkedBean("publishing:nextPage", HippoBean.class);
+    }
+
+}
