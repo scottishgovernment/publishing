1e79832baa8ce172b74cd6061167c964eafa45d7
David Sinclair <david.sinclair@gov.scot>
2022-10-07 14:29:42 +0100

    MGS-6975 shared populateAutoCompleteFlag in header, home and search pages

--- site/components/src/main/java/scot/mygov/publishing/components/SearchComponent.java
+++ site/components/src/main/java/scot/mygov/publishing/components/SearchComponent.java
@@ -3,19 +3,14 @@ package scot.mygov.publishing.components;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
 import org.onehippo.cms7.essentials.components.EssentialsContentComponent;
-import scot.gov.publishing.hippo.funnelback.component.ResilientSearchComponent;
-import scot.gov.publishing.hippo.funnelback.component.SearchSettings;
+
+import static scot.mygov.publishing.components.SiteHeaderComponent.populateAutoCompleteFlag;
 
 public class SearchComponent extends EssentialsContentComponent {
 
     public void doBeforeRender(HstRequest request, HstResponse response) {
         super.doBeforeRender(request, response);
-        SearchSettings searchsettings = ResilientSearchComponent.searchSettings();
-        request.setAttribute("autoCompleteEnabled", autoCompleteEnabled(searchsettings));
-    }
-
-    boolean autoCompleteEnabled(SearchSettings searchSettings) {
-        return searchSettings.isEnabled() && !"bloomreach".equals(searchSettings.getSearchType());
+        populateAutoCompleteFlag(request);
     }
 
 }
