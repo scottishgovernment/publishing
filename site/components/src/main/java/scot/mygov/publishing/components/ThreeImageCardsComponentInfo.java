7ffe844150e13d3f0574d00fceb9e037b60ac66a
David Kindness <david.kindness@gov.scot>
2022-10-25 13:46:21 +0100

    MGS-6976 update cards content block
    add neutral link option
    add grey card option
    update template to match DS and reduce repetition
    group fields in CMS

--- site/components/src/main/java/scot/mygov/publishing/components/ThreeImageCardsComponentInfo.java
+++ site/components/src/main/java/scot/mygov/publishing/components/ThreeImageCardsComponentInfo.java
@@ -1,11 +1,18 @@
 package scot.mygov.publishing.components;
 
+import org.hippoecm.hst.core.parameters.FieldGroup;
+import org.hippoecm.hst.core.parameters.FieldGroupList;
 import org.hippoecm.hst.core.parameters.JcrPath;
 import org.hippoecm.hst.core.parameters.Parameter;
 
 import static scot.mygov.publishing.components.ThreeImageCardsComponent.CMS_PICKERS_DOCUMENTS_ONLY;
 import static scot.mygov.publishing.components.ThreeImageCardsComponent.INITIAL_PATH;
 import static scot.mygov.publishing.components.ThreeImageCardsComponent.TYPE;
+
+@FieldGroupList({
+    @FieldGroup(titleKey = "Appearance", value = { "greycards", "fullwidth", "showimages", "small", "neutrallinks" }),
+    @FieldGroup(titleKey = "Content", value = { "document1", "document2", "document3" })
+})
 
 public interface ThreeImageCardsComponentInfo {
 
@@ -39,6 +46,12 @@ public interface ThreeImageCardsComponentInfo {
     @Parameter(name = "showimages", displayName = "Show images", defaultValue = "true")
     Boolean getShowImages();
 
-    @Parameter(name = "small", displayName = "Use small variant on mobile")
+    @Parameter(name = "small", displayName = "Use small variant on mobile", defaultValue = "true")
     Boolean getSmallVariant();
+
+    @Parameter(name = "neutrallinks", displayName = "Neutral link colour", defaultValue = "false")
+    Boolean getNeutralLinks();
+
+    @Parameter(name = "greycards", displayName = "Grey cards on white background", defaultValue = "false")
+    Boolean getGreyCards();
 }
