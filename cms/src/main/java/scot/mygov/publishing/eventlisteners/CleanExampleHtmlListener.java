fe7de4ffdbe3a0a1bc3093135b0998f6ceb32f10
David Sinclair <david.sinclair@gov.scot>
2023-07-11 11:20:09 +0100

    MGS-7294 use jsoupt to clean ds examples
    uses jsoup with a custom safelist to clean the html elements we allow
    in ds example code.
    
    we allow script tags in this safelist, but then use a node visitor to remove any
    whose type is not application/ld+json

---
+++ cms/src/main/java/scot/mygov/publishing/eventlisteners/CleanExampleHtmlListener.java
@@ -0,0 +1,120 @@
+package scot.mygov.publishing.eventlisteners;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.safety.Safelist;
+import org.onehippo.cms7.services.eventbus.Subscribe;
+import org.onehippo.repository.events.HippoWorkflowEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scot.mygov.publishing.HippoUtils;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import static org.apache.commons.lang3.StringUtils.equalsIgnoreCase;
+import static scot.mygov.publishing.eventlisteners.EventListerUtil.ensureRefreshFalse;
+
+public class CleanExampleHtmlListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CleanExampleHtmlListener.class);
+
+    private static final String CODE = "publishing:code";
+
+    private static final String SCRIPT = "script";
+
+    Session session;
+
+    // defines the html elements that we want to allow in examples.
+    // we allow script tags so that we can allow non executable script tags.
+    // we then use a node visitor to remove executable ones.
+    Safelist safelist = Safelist.relaxed()
+            .addTags(SCRIPT, "address","fieldset","footer","form","header","input","label","legend",
+                    "main","nav","option","section","select","style","svg","textarea","use")
+
+            .addAttributes(SCRIPT, "type")
+            .addAttributes(":all","aria-describedby","aria-invalid","aria-label","aria-labelledby",
+                    "aria-required","class","data-module","id","role","tabindex","translate")
+            .addAttributes("button","disabled","type")
+            .addAttributes("form","action","method")
+            .addAttributes("img","loading","sizes","srcset")
+            .addAttributes("input","aria-autocomplete","aria-expanded","aria-owns","autocomplete",
+                    "checked","data-behaviour","haspopup","maxlength","name","placeholder","required",
+                    "type","value","hidden")
+            .addAttributes("label","for")
+            .addAttributes("ol","data-total","start")
+            .addAttributes("option","selected")
+            .addAttributes("svg","aria-hidden")
+            .addAttributes("table","data-smallscreen")
+            .addAttributes("textarea","rows")
+            .addAttributes("use","href")
+
+            .addProtocols("use","href","http","https")
+            .addProtocols("img","srcset","http","https");
+
+    CleanExampleHtmlListener(Session session) {
+        this.session = session;
+    }
+
+    @Subscribe
+    public void handleEvent(HippoWorkflowEvent event) {
+
+        if (!canHandle(event)) {
+            return;
+        }
+        try {
+            doHandleEvent(event);
+        } catch (RepositoryException e) {
+            ensureRefreshFalse(session);
+            LOG.error(
+                    "error trying to clean code for event msg={}, action={}, event={}, result={}", e.getMessage(),
+                    event.action(), event.category(), event.result(), e);
+        }
+    }
+
+    boolean canHandle(HippoWorkflowEvent event) {
+        return "publishing:dsexample".equals(event.documentType())
+                && "commitEditableInstance".equals(event.action());
+    }
+
+    void doHandleEvent(HippoWorkflowEvent event) throws RepositoryException {
+        Node handle = session.getNode(event.subjectPath());
+        new HippoUtils().apply(handle.getNodes(handle.getName()), this::cleanCode);
+        session.save();
+    }
+
+    void cleanCode(Node node) throws RepositoryException {
+        String code = node.getProperty(CODE).getString();
+        String cleanedCode = Jsoup.clean(code, safelist);
+        String withoutExecutableScript = removeExecutableScript(cleanedCode);
+        node.setProperty(CODE, withoutExecutableScript);
+    }
+
+    String removeExecutableScript(String code) {
+        Document doc = Jsoup.parse(code);
+        doc.body().traverse(this::visit);
+        return doc.body().html();
+    }
+
+    void visit(org.jsoup.nodes.Node node, int depth) {
+        if (isExecutableScript(node)) {
+            node.remove();
+        }
+    }
+
+    boolean isExecutableScript(org.jsoup.nodes.Node node) {
+        if (!(node instanceof Element)) {
+            return false;
+        }
+
+        Element element = (Element) node;
+        if (!equalsIgnoreCase(element.tagName(), SCRIPT)) {
+            return false;
+        }
+
+        String type = element.attr("type");
+        return !"application/ld+json".equalsIgnoreCase(type);
+    }
+}
