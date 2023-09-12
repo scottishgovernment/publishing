aa8cb4b8091b22c4e434a7703f5dca193f79e69e
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2023-09-12 13:47:33 +0100

    MGS-7413 Add JS listener to determine click type and modifier keys (bump PL version to take new script) MGS-7387 Add "section" to DataLayer for clicks (bump PL version to take new script) MGS-7172 Google cookies should be deleted if user revokes cookie consent (new PL version and publishing script changes)

--- repository-data/webfiles/src/main/scripts/components/cookie-form.js
+++ repository-data/webfiles/src/main/scripts/components/cookie-form.js
@@ -76,8 +76,38 @@ const cookieForm = {
             cookieNotice.classList.add('notification--confirmed');
             cookieNotice.parentNode.querySelector('.js-initial-cookie-content').classList.add('fully-hidden');
             cookieNotice.parentNode.querySelector('.js-confirm-cookie-content').classList.remove('fully-hidden');
-        });
 
+            cookieForm.removeDisallowedCookies();
+
+        });
+    },
+
+    removeDisallowedCookies: function() {
+        // remove cookies
+        // this is ugly
+        const cookiesAsObject = document.cookie.split('; ').reduce((prev, current) => {
+            const [name, ...value] = current.split('=');
+            prev[name] = value.join('=');
+            return prev;
+        }, {});
+
+        if (!storage.hasPermission(storage.categories.statistics)) {
+            const statisticsCookiePrefixes = ['_ga', '_gid', '_ga_'];
+            for (const cookie in cookiesAsObject) {
+                if (statisticsCookiePrefixes.includes(cookie.substring(0,4))) {
+                    storage.cookie.remove(cookie);
+                }
+            }
+        }
+
+        if (!storage.hasPermission(storage.categories.preferences)) {
+            const preferencesCookiePrefixes = ['banner-'];
+            for (const cookie in cookiesAsObject) {
+                if (preferencesCookiePrefixes.includes(cookie.substring(0,8))) {
+                    storage.cookie.remove(cookie);
+                }
+            }
+        }
     }
 };
 
