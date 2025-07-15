51dec998985ce6a9b0955af53ee68c70f11a6ba3
David Sinclair <david.sinclair@gov.scot>
2025-07-15 15:53:49 +0100

    Use breabcrumbs to decide what menu item to expand

--- site/components/src/main/java/scot/mygov/publishing/components/MenuComponent.java
+++ site/components/src/main/java/scot/mygov/publishing/components/MenuComponent.java
@@ -1,26 +1,26 @@
 package scot.mygov.publishing.components;
 
 import org.apache.commons.lang.StringUtils;
-import org.hippoecm.hst.content.beans.standard.HippoBean;
-import org.hippoecm.hst.content.beans.standard.HippoFolder;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
-import org.hippoecm.hst.core.linking.HstLink;
-import org.hippoecm.hst.core.linking.HstLinkCreator;
 import org.hippoecm.hst.core.parameters.ParametersInfo;
 import org.hippoecm.hst.core.sitemenu.HstSiteMenu;
 import org.hippoecm.hst.core.sitemenu.HstSiteMenuItem;
 import org.hippoecm.hst.core.sitemenu.HstSiteMenuItemImpl;
 import org.onehippo.cms7.essentials.components.EssentialsMenuComponent;
 import org.onehippo.cms7.essentials.components.info.EssentialsMenuComponentInfo;
+import org.onehippo.forge.breadcrumb.om.BreadcrumbItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import static scot.mygov.publishing.components.BreadcrumbComponent.constructBreadcrumb;
 
 @ParametersInfo(type = EssentialsMenuComponentInfo.class)
 public class MenuComponent extends EssentialsMenuComponent {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MenuComponent.class);
 
     @Override
     public void doBeforeRender(final HstRequest request, final HstResponse response) {
@@ -31,66 +31,26 @@ public class MenuComponent extends EssentialsMenuComponent {
             return;
         }
 
-        HstLink link = getLink(request);
-        Map<String, String> proxymap = getProxyMap(request);
+        List<BreadcrumbItem> breadcrumbs = constructBreadcrumb(request, request.getRequestContext().getContentBean());
         List<HstSiteMenuItem> menuItems = menu.getSiteMenuItems();
         for (HstSiteMenuItem menuItem : menuItems) {
-            setExpanded(menuItem, request, link, proxymap);
+            setExpanded(menuItem, breadcrumbs);
         }
     }
 
-    HstLink getLink(HstRequest request) {
-        HippoBean contentBean = request.getRequestContext().getContentBean();
-        if (contentBean == null) {
-            return null;
-        }
-        HstLinkCreator linkCreator = request.getRequestContext().getHstLinkCreator();
-        return linkCreator.create(contentBean, request.getRequestContext());
-    }
 
-    void setExpanded(HstSiteMenuItem menuItem, HstRequest request, HstLink link, Map<String, String> proxymap) {
+    void setExpanded(HstSiteMenuItem menuItem, List<BreadcrumbItem> breadcrumbs) {
         HstSiteMenuItemImpl itemImpl = (HstSiteMenuItemImpl) menuItem;
+        for (BreadcrumbItem breadcrumb : breadcrumbs) {
+            if (StringUtils.isEmpty(breadcrumb.getLink().getPath())) {
+                // ignore the home page in the breadcrumb
+                continue;
+            }
 
-        // hande case where a guide is the menu item
-        String itemPath = itemImpl.getHstLink().getPath();
-        if (link != null && link.getPath().startsWith(itemPath) && StringUtils.isNotBlank(itemPath)) {
-            itemImpl.setExpanded();
-        }
-
-        String proxyPath = proxymap.get(itemPath);
-        HippoBean contentBean = request.getRequestContext().getContentBean();
-        if (contentBean == null || proxyPath == null) {
-            return;
-        }
-
-        if (contentBean.getPath().startsWith(proxyPath)) {
-            itemImpl.setExpanded();
-        }
-    }
-    /**
-     * For the top level categories of the site, determine the link for any categories with a 'breadcrumbProxy'*
-     */
-    private Map<String, String> getProxyMap(HstRequest request) {
-        HippoBean base = request.getRequestContext().getSiteContentBaseBean();
-        HippoFolder browse = base.getBean("browse");
-        if (browse == null) {
-            return Collections.emptyMap();
-        }
-
-        HstLinkCreator linkCreator = request.getRequestContext().getHstLinkCreator();
-        List<HippoFolder> folders = browse.getChildBeans(HippoFolder.class);
-        Map<String, String> map = new HashMap<>();
-        for (HippoFolder folder : folders) {
-            HippoBean category = folder.getBean("index");
-            if (category != null) {
-                HippoBean proxy = category.getLinkedBean("publishing:breadcrumbProxy", HippoBean.class);
-                if (proxy != null) {
-                    HstLink link = linkCreator.create(proxy, request.getRequestContext());
-                    map.put(link.getPath(), category.getParentBean().getPath());
-                }
+            if (breadcrumb.getLink().getPath().equals(itemImpl.getHstLink().getPath())) {
+                itemImpl.setExpanded();
             }
         }
-        return map;
     }
 
 }
