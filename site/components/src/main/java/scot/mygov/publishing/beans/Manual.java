8d44da53bccf2998eeadfeaca875f5d198ea96d2
David Sinclair <david.sinclair@gov.scot>
2025-09-16 16:15:04 +0100

    MGS-8069 fix manual label

---
+++ site/components/src/main/java/scot/mygov/publishing/beans/Manual.java
@@ -0,0 +1,39 @@
+package scot.mygov.publishing.beans;
+
+import org.hippoecm.hst.container.RequestContextProvider;
+import org.hippoecm.hst.content.beans.Node;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import scot.mygov.publishing.components.PublicationTypesProvider;
+
+import java.util.Calendar;
+import java.util.Map;
+
+@HippoEssentialsGenerated(internalName = "publishing:Manual")
+@Node(jcrType = "publishing:Manual")
+public class Manual extends Base {
+    @HippoEssentialsGenerated(internalName = "publishing:publicationDate")
+    public Calendar getPublicationDate() {
+        return getSingleProperty("publishing:publicationDate");
+    }
+
+    @HippoEssentialsGenerated(internalName = "publishing:contentitemlanguage")
+    public String getContentitemlanguage() {
+        return getSingleProperty("publishing:contentitemlanguage");
+    }
+
+    @HippoEssentialsGenerated(internalName = "publishing:topics")
+    public String[] getTopics() {
+        return getMultipleProperty("publishing:topics");
+    }
+
+    @HippoEssentialsGenerated(internalName = "publishing:publicationType")
+    public String getPublicationType() {
+        return getSingleProperty("publishing:publicationType");
+    }
+
+    public String getLabel() {
+        Map<String, String> publicationTypes = new PublicationTypesProvider(false).get(RequestContextProvider.get());
+        String publicationType = getPublicationType();
+        return publicationTypes.containsKey(getPublicationType()) ? publicationTypes.get(publicationType) : "Publication";
+    }
+}
